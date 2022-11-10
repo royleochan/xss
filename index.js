05b99ffc7edbfa0d59cadbbf8e47504a41f7036a
Roy Chan <royleochan@gmail.com>
2022-11-10 17:22:23 +0800

    fix: persistent xss

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ async function reqListener(req, res) {
   const url = req.url;
   const method = req.method;
 
-if (url === "/post" && method == "GET") {
+  if (url === "/post" && method == "GET") {
     const posts = await db.getPosts();
     res.writeHead(200, { "content-type": "application/json" });
     res.write(JSON.stringify(posts));
     res.end();
-} else if (url === "/post/create" && method == "POST") {
+  } else if (url === "/post/create" && method == "POST") {
     var formData = "";
 
     req.on("data", function (data) {
@@ -23,16 +23,16 @@ if (url === "/post" && method == "GET") {
     req.on("end", async function () {
       const fields = formData.split("\n");
       const title = fields[0].split("=")[1];
-      const content = fields[1].split("=")[1];
+      let index = fields[1].indexOf("=");
+      const content = fields[1].slice(index + 1, fields[1].length);
+
       await db.addPost(title, content);
       res.end();
     });
-} else {
-  res.writeHead(200, { "content-type": "text/html" });
-  fs.createReadStream("public/index.html").pipe(res);
-} 
-
-
+  } else {
+    res.writeHead(200, { "content-type": "text/html" });
+    fs.createReadStream("public/index.html").pipe(res);
+  }
 }
 
 const server = http.createServer(reqListener);
